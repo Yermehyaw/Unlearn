2e7628395bb89bae897ae80f37d124cee703e6a9
Yermehyaw <yermehyaw@gmail.com>
2024-08-28 08:23:33 -0700

    Add initializer

--- models/students.py
+++ models/students.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """
-BaseModel class from which all classes Inherit
+Studrnt class from which defines all student users
 
 Modules Imported:
+bcrypt(func) - encrypt a password string
+uuid4(method) - Generates a random UUID
 """
+import bcrypt
+from uuid import uuid4  # funtion
 
 
 class Students():
@@ -11,9 +15,32 @@ class Students():
     Defines a student user
 
     Args:
-    None
+    name(str): student name
+    username: login username
+    password(str): login password
+
 
     Attributes:
-    
+    student_id(int): unique student id
+    name(str): student's name
+    username(str): login username
+    p_hash(str): hash value of password
+    creaated_at(timestamp): timestamp student user was created
     """
-    
+
+
+
+    def __init__(self, name=None, username, password):
+        """
+        Class initializer
+        """
+        student_id = uuid4()
+        name = name
+
+        if username == None:
+            raise ValueError('Pls enter a valid username')
+        else if username:  # == any username already in storage
+            pass  # edit when storage_db is added
+
+        __salt = bcrypt.gensalt()
+        p_hash = bcrypt.hashpw(password.encode('utf-8'), salt)
