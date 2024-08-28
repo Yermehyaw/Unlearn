a521698c4832953fb00a846c31968513203c479b
Yermehyaw <yermehyaw@gmail.com>
2024-08-28 03:08:13 -0700

    Add student class

---
+++ models/students.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+BaseModel class from which all classes Inherit
+
+Modules Imported:
+"""
+
+
+class Students():
+    """
+    Defines a student user
+
+    Args:
+    None
+
+    Attributes:
+    
+    """
+    
