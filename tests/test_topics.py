77a4dcaf3debe42e8969a8ffe362e95ba835ad86
Yermehyaw <yermehyaw@gmail.com>
2024-08-30 19:11:52 -0700

    Add add_lesson test

--- tests/test_topics.py
+++ tests/test_topics.py
@@ -3,13 +3,13 @@
 Test Students class
 
 Modules/Functs Imported:
-pycodestyle: PEP8 coding linter
 unittest: create unittests for python scripts
-
+Lessons(class) - create a new lesson
+Topics(class) - create a new topic
 """
-import pycodestyle
-import unittest
+from models.lessons inport Lessons
 from models.topics import Topics
+import unittest
 
 
 class TestTopics(unittest.TestCase):
@@ -32,3 +32,31 @@ class TestTopics(unittest.TestCase):
         self.assertEqual(monosaccharides.course_title, 'carbohydrates')
         self.assertNotEqual(monosaccharides.course_desc, '')
         self.assertIsNotNone(monosaccharides.topic_desc)
+
+    def test_add_lesson(self):
+        """Tests add_lesson() in adding a new lesson"""
+        obj_topic = Topics(
+            210,
+            'monosaccharides',
+            'carbohydrates',
+            'sugars in biochemistry',
+            'one molecule sugars'
+        )
+        obj_topic.add_lesson(  # add a new lesson
+            'learn carbohydrates',
+            'this is how to eaily learn carbohydrates',
+            'helping students learn carbohydrates'
+        )
+        obj_lesson = Lessons(  # create a duplicate of the object
+            'learn carbohydrates',
+            'this is how to eaily learn carbohydrates',
+            'helping students learn carbohydrates'
+        )
+
+        for key, value in obj_topic.lessons():
+            if key != 'lesson_id':
+                self.assertEqual(value, obj_lesson.key)
+        self.assertNotEqual(
+            obj_topic.lessons['lesson_id'],
+            obj_lesson.lesson_id
+        )
