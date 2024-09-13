3618aed380c828aa1f719308c62c96a331873593
Yermehyaw <yermehyaw@gmail.com>
2024-09-13 16:00:09 +0100

    Addd

--- models/lessons.py
+++ models/lessons.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
-Mini tutorial lessons for each topic
+Mini tutorial lessons with quiz for each topic
 
 Modules Imported:
 uuid4(method) - generate unique ids
+Questions(class) - question bank
 
 """
 from uuid import uuid4
+from questions import Questions
 
 
 class Lessons:
@@ -45,3 +47,47 @@ class Lessons:
                 raise TypeError('Invalid lesson description')
             else:
                 self.lesson_desc = lesson_desc
+
+    class Quiz:
+        """
+        Interface for answering questions after a lesson. Creates
+        a dynamic quiz session
+
+        Args:
+        None
+
+        Attributes:
+        quiz_id(int): unique id of a quiz
+        quiz_name(str): name of quiz
+        quiz_type(str): type of quiz e.g MCQ, T/F
+        score(int): score on a quiz session
+
+        """
+        def __init__(self, quiz_name, quiz_type=None):
+            """Class initializer"""
+            self.quiz_id = uuid4()
+
+            # also check if quiz nane already exisrs in the db
+            if not isinstance(quiz_name, str):
+                raise TypeError('Invalid quiz name')
+            else:
+                self.quiz_name = quiz_name
+
+            if not isinstance(quiz_type, str):
+                raise TypeError('Invalid quiz type')
+            else:
+                self.quiz_type = quiz_type
+
+            self.score = 0
+
+        def new_quiz_session(self):
+            """
+            Prepares a new quiz session for a user
+
+            Args:
+            None
+
+            """
+            questions = Questions.get_questions() ###
+
+            return
