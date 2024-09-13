eee37b5ab9292bc5ddb7a3f1165c60af1bab50a2
Yermehyaw <yermehyaw@gmail.com>
2024-09-13 12:28:59 -0700

    use unittests discover

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # pls set working dir to Unlearn/
-echo "pls set working dur to: Unlearn/"
-python3 -m unittest tests/*
+echo "pls set working dir to: Unlearn/"
+python3 -m unittest discover tests/
