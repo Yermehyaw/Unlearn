45c9901ebf030f9c242bb2c678ddc42d1480b8ed
Yermehyaw <yermehyaw@gmail.com>
2024-09-15 00:39:00 +0100

    Add profile route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,13 +11,35 @@ from views import app_views
 from flask import jsonify
 
 
-
 @app_views.route('/login', strict_slashes=False)
 def login():
     """Receieve user login for authentication"""
     pass
 
+
 @app_views.route('/status/<student_id>', strict_slashes=False)
 def status(student_id):
     """Return the login status of a student user"""
-    pass
+    # if a not valid student id from storage
+    status = {'Logged In': False}
+    # else
+    # if <condition for login of a studnent>
+    status['Logged In'] = True
+    return jsonify(status)
+
+
+@app_views.route('/profile/<student_id>', strict_slashes=False)
+def profile(student_id):
+    """Return the details of a student"""
+    # if student exists in storage
+    # get student_id, name, username and created_at details from the
+    # student object retrieved from storage
+    student_details = {
+        'student_id': '',
+        'username': '',
+        'name': '',
+        'created_at': ''
+    }
+    return jsonify(student_details)
+    # else
+    return jsonify({'response': 'invalid credential'))
