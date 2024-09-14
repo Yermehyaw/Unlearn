e58f5551b11815e0174b8d4fe74e38784aaefa0b
Yermehyaw <yermehyaw@gmail.com>
2024-09-14 12:01:42 +0100

    Add teardown context and more imports

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,14 +3,28 @@
 Modules Imported: Flask
 Flask: web app franework
 swagger: flasgger's web app testing and documen interface
+CORS: allows cross prigin resouece sharing across diff endpoints
 
 """
 from flask import Flask
+from flask_cors import CORS
 from flasgger import Swagger
 
 
-app = Flask(__name__, instance_relative_config=False)
+app = Flask(__name__)
+app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
 swagger = Swagger(app)
+
+# cors activation
+
+
+@app.teardown_appcontext
+def close_app():
+    """Closes storage when app is closed"""
+    pass
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
