e00387feb0f0e55b252b94ff5ae4bfbab6c31b4f
Yermehyaw <yermehyaw@gmail.com>
2024-09-16 11:13:54 +0100

    Add question getter and setter methods

--- models/questions.py
+++ models/questions.py
@@ -4,7 +4,6 @@ Question bank for the unlearn app
 
 Modules Imported:
 uuid4(method) - generate unique ids
-UUID(cls) - base class for all uuid
 
 """
 from uuid import uuid4
@@ -30,7 +29,7 @@ class Questions:
     option_selection(dict): selectable options each with an
     individual option_id and option_text
     selected_option(list): option_id and option_text of the selected option
-    correct_option(str): option_id and option_text of the corrrect option
+    correct_option(list): option_id and option_text of the corrrect option
 
     """
     def __init__(self, question_str, tip=None):
@@ -53,13 +52,107 @@ class Questions:
         self._useful_in_topic_name = []
 
         if not isinstance(option_type, str):
-            raise TypeError('Invalid option type,  enter "mcq" or "t/f"')
+            raise TypeError('Invalid option type')
+        elif option_type != 'mcq' or option_type != 't/f':
+            raise TypeError('Invalid option type,  pls enter "mcq" or "t/f"')
         else:
             self.option_type = option_type
 
         self.option_selection = {}
 
+        self.selected_option = []
 
+        self.correct_option = []
+
+
+    @property
+    def show_question(self):
+        """Get the question with its options in a user friendly format"""
+        self.reload()
+
+        pretty_question = {'question': question_str}
+
+        if self.option_type == 'mcq':
+            pretty_option = {
+                'option_type': 'Multiple Choice Question',
+                'options': [
+                    self.option_selection['A'],
+                    self.option_selection['B'],
+                    self.option_selection['C'],
+                    self.option_selection['D'],
+                    self.option_selection['E']
+                ],
+                'correct_option': self.correct_option[1]
+                # index 1 is the option_text
+            }
+        elif self.option_type == 't/f':
+            pretty_option = {
+                'option_type': 'True or False Question',
+                'options': [
+                    self.option_selection['True'],
+                    self.option_selection['False']
+                ],
+                'correct_option': self.correct_option[1]
+            }
+
+        final_pretty_q = pretty_question.update(pretty_option)
+        return final_pretty_q
+
+
+    @show_question.setter
+    def edit_question(
+            self,
+            question_str,
+            option_type,
+            option_to_change,
+            change_option_to,
+    ):
+        """
+        Edit the attributes of an already existing questions object
+
+        Args:
+        question_str(str): new question string
+        option_type(str): new_option_type. May be the same option
+        type or a diff one
+        option_to_change(str): A, B, C, D E. Only applicable if
+        option_type is "mcq"
+        change_option_to(str): string to change the selected option into
+
+        NOTE: Remember to call sace() after any operation/edit is dobe
+        this function does not save changes automatically to storage
+        """
+        self.reload()
+
+        if not isinstance(question_str, str):
+            raise TypeError('Invalid new question')
+        else:
+            self.question_str = question_str
+
+        if self.option_type == 'mcq' and option_type == 't/f':
+            # if the original option_type was mcq but user wishes to
+            # change it to 't/f'
+            option_true = 'option_' + str(uuid4().int)
+            option_false = 'option_' + str(uuid4().int)
+            self.option_selection = {
+                'True': [True, option_true],
+                'False': [False, option_false]
+            }
+        elif self.option_type == 't/f' and option_type == 'mcq':
+            # if the original option_type was t/f but now being changed
+            # to mcq
+            new_option_id = 'option_' + str(uuid4().int)
+
+            if option_to_change == 'A':  # use switch case statement
+                self.option_selection = {
+                    'A': [change_option_to, new_option_id]
+                }
+            elif option_to_change == 'B':
+                # continue from here
+
+        elif self.option_type == 'mcq' and option_type == 'mcq':
+
+        elif self.option_type == 't/f' and option_type == 't/f':
+                pass
 
     def useful_in(self, get_ids=True):
         """
@@ -100,5 +193,3 @@ class Questions:
     def save(self):
         """Save object to storage"""
             # Coming soon. . .
-
-            
