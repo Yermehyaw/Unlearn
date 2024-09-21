ca13456658482f9f6785dbf9c1feecaf4f522f65
Yermehyaw <yermehyaw@gmail.com>
2024-09-21 17:44:34 +0100

    Fix code struct

--- api/v1/views/courses.py
+++ api/v1/views/courses.py
@@ -31,27 +31,29 @@ def spec_courses(course_code):
     """Returns the details on a specified course code"""
     if request.method == 'GET':
         # if valid/existing course code
+        # retrieve from storage and init to req_course
         course_details = {
-            'course_code': ''
-            'course_title': '',
-            'course_desc': '',
-            'no_topics': ''
+            'course code': 'req_course.course_code'
+            'course title': 'req_course.course_title',
+            'description': 'req_course.course_desc',
+            'no of topics': 'req_course.get_topics_no',
+            'list topics': 'req_course.get_all_topics_list'
         }
-        # 'list topics': []
         return jsonify(course_details)
-    if request.method == 'POST':
-        # if course code dosent exist
-        new_course = Courses(course_code)
-        form_data = request.form  # attain any data via a form (not json)
-        if form_data:
-            for key, value in form_data.items():
-                if key == 'course_title' or key == 'course_name':
-                    new_course.course_title = value
-                if key == 'course_desc' or key == 'description':
-                    new_course.course_desc = value
-                if key == 'no_topics':
-                    new_course.no_topics = value
-        # add to storage using save(): new_course.save() (?)
-        return jsonify({'response': 'course created successfully'})
-        # else:
-        return jsonify({'response': 'course creation failed'})
+
+    elif request.method == 'POST':
+        if course code dosent exist:
+            new_course = Courses(course_code)
+            form_data = request.form  # attain any data via a form (not json)
+            if form_data:
+                for key, value in form_data.items():
+                    if key == 'course_title' or key == 'course_name':
+                        new_course.course_title = value
+                    if key == 'course_desc' or key == 'description':
+                        new_course.course_desc = value
+                    if key == 'no_topics':
+                        new_course.no_topics = value
+        # add to storage using save() via new_course.save()
+            return jsonify({'response': 'course created successfully'})
+        else:  # course found in storage
+            return jsonify({'response': 'course already exists'})
