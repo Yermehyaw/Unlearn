b26a059ee2580d56bac83dd7537a3ee2bbbaa3bc
Yermehyaw <yermehyaw@gmail.com>
2024-09-21 17:56:53 +0100

    template file

---
+++ api/v1/views/raw_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+REST api for . . . requests
+
+Modules Imported: api_views, jsonify
+
+api_views: flask api blueprint
+jsonify: convert response to valid json
+
+
+"""
+from views import api_views
+
+from flask import jsonify
+from flask import request
+
+
+@app_views.route('/', strict_slashes=False)
+def :
+
+
+@app_views.route(
+    '/',
+    methods=['GET', 'POST'],
+    strict_slashes=False
+)
+def :
