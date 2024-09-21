4918b0e60b708ff6698bb3e385df4b7fa169914d
Yermehyaw <yermehyaw@gmail.com>
2024-09-21 20:05:39 +0100

    finish endpoints

--- api/v1/views/topics.py
+++ api/v1/views/topics.py
@@ -26,13 +26,33 @@ def all_topics_count():
     return jsonify(total)
     
 @app_views.route(
-    '/topics/<topic_title>',
+    '/topics/<course_title>',
     methods=['GET', 'POST'],
     strict_slashes=False
 )
-def all_topics_list:
+def all_topics_list(course_title):
     """Returns a list of all topics of a course title
     or creates a new course"""
     if request.method == 'GET':
         # check if its a valid course code from storage
-
+        # retrieve coourse obj from storage as req_course
+        course_topics = {
+            'course code': 'req_course.course_code',
+            'course title': 'req_course.course_title'
+            'topics': 'req_course.get_all_topics_list'
+        }
+        return jsonify(course_topics)
+    elif request.method == 'POST':
+        if 1:  # course exists
+            form_data = request.form_data
+            if form_data:
+                for key, value in form_data.items():
+                    if key == 'topic title' or key == 'topic name':
+                        # filter input during security implement
+                        new_topic_title = value
+            else:
+                return jsonify({'response': 'No form data sent'})
+            new_topic = 'req_course.add_topic(new_topic_title)'
+            return jsonify({'response': 'topoc created successfully'})
+        else:  # course dosent exist, course must exist to have a topic
+            return jsonify({'response': 'course not found'})
