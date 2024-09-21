1906633440a823ec132778d9964528a25de868d2
Yermehyaw <yermehyaw@gmail.com>
2024-09-21 18:14:18 +0100

    Add /topics/<topic_title> endpoint

--- api/v1/views/topics.py
+++ api/v1/views/topics.py
@@ -2,29 +2,37 @@
 """
 REST api for course topic requests
 
-Modules Imported: app_views, jsonify, Topics
+Modules Imported: app_views, SQLALchemy, jsonify, Topics
 
 app_views: flask api blueprint
 jsonify: convert response to valid json
 Topics(cls): Topics class
+SQLAlchemy: ORM for flask apps
 
 """
 from views import app_views
-from modeks.topics import Topics
+from models.topics import Topics
 from flask import jsonify
 from flask import request
+from flask_sqlalchemy import SQLALchemy
 
 
 @app_views.route('/topics', strict_slashes=False)
 def all_topics_count():
     """Returns a dict of all topics"""
-    # get total no of topics stored in db
+    # get total no of topics stored in storage
+    total = 0  # change when retrieved from storage
+    total = {'Total topics': total}
+    return jsonify(total)
     
-
-
 @app_views.route(
-    '/',
+    '/topics/<topic_title>',
     methods=['GET', 'POST'],
     strict_slashes=False
 )
-def :
+def all_topics_list:
+    """Returns a list of all topics of a course title
+    or creates a new course"""
+    if request.method == 'GET':
+        # check if its a valid course code from storage
+
