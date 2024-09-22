26adafe4689f14f78348e00ae1a5d541ab480214
Yermehyaw <yermehyaw@gmail.com>
2024-09-22 01:51:37 +0100

    Fix /quiz/course_title/topic_title route

--- api/v1/views/quiz.py
+++ api/v1/views/quiz.py
@@ -22,53 +22,44 @@ from models.questions import Questions
 @app_views.route('/quiz/<course_title>', strict_slashes=False)
 def all_course_questions(course_title):
     """Returns a dict of all questions in a course"""
-    # If course exists in storage, save as req_course
-    topics_in_course = 'req_course.get_all_topics_list'  # a list
-    topics_in_course = []  # placeholder, remove
-    course_questions = {'course title': 'req_course.course title'}
-    for topic in topics_in_course:
-        questions_in_topic = topic.questions
-        q_list = []
-        for question in questions_in_topic:
-            question_str = question.question_str
-            q_list.append(question_str)
-        topic_q = {topic.topic_title: q_list}
-        course_questions.update(topic_q)
-    return jsonify(course_questions)
+    if 1: #  course exists in storage, save as req_course
+        topics_in_course = 'req_course.get_all_topics_list'  # a list
+        topics_in_course = []  # placeholder, remove
+        course_questions = {'course title': 'req_course.course title'}
+        for topic in topics_in_course:
+            questions_in_topic = topic.questions
+            q_list = []
+            for question in questions_in_topic:
+                question_str = question.question_str
+                q_list.append(question_str)
+            topic_q = {topic.topic_title: q_list}
+            course_questions.update(topic_q)
+        return jsonify(course_questions)
+    else:
+        return jsonify({})
 
 @app_views.route(
     '/quiz/<course_title>/<topic_title>',
     methods=['GET'],
     strict_slashes=False
 )
-def spec_courses(course_title, topic_title):
-    """Returns the details on a specified course code"""
-    if request.method == 'GET':
-        # if valid/existing course code
-        # retrieve from storage and init to req_course
-        course_details = {
-            'course code': 'req_course.course_code'
-            'course title': 'req_course.course_title',
-            'description': 'req_course.course_desc',
-            'no of topics': 'req_course.get_topics_no',
-            'list topics': 'req_course.get_all_topics_list'
-        }
-        return jsonify(course_details)
+def spec_topic_questions(course_title, topic_title):
+    """Returns all questions on a specified topic"""
+    if 1:  # course exists, init to req_course
+        topics_in_course = 'req_course.get_all_topics_list'  # a list
+        topics_in_course = []  # placeholder, remove
+        course_questions = {'course title': 'req_course.course title'}
+        for topic in topics_in_course:
+            if topic.topic_title == topic_title:  # if == to the one user requ
+                req_topic = topic  # found the spec requested topic
+                questions_in_topic = topic.questions
+                q_list = []
+                for question in questions_in_topic:
+                    question_str = question.question_str
+                    q_list.append(question_str)
+                topic_q = {topic.topic_title: q_list}
+                course_questions.update(topic_q)
+        return jsonify(course_questions)
 
-    elif request.method == 'POST':
-        if 1:  # course already exists:
-             return jsonify({'response': 'course already exists'})
-        else:  # course not  found in storage
-            new_course = Courses(course_code)
-            form_data = request.form  # attain any data via a form (not json)
-            if form_data:
-                for key, value in form_data.items():
-                    if key == 'course_title' or key == 'course_name':
-                        new_course.course_title = value
-                    if key == 'course_desc' or key == 'description':
-                        new_course.course_desc = value
-                    if key == 'no_topics':
-                        new_course.no_topics = value
-        # add to storage using save() via new_course.save()
-        resp = jsonify({'response': 'course created successfully'})
-        return make_response(resp, 201)
+    else:  # course not found in storage
+        return jsonify({})
