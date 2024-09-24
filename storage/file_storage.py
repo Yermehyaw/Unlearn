4da76c2198017402bf0fb33e696cb4608d62e182
Yermehyaw <yermehyaw@gmail.com>
2024-09-24 17:18:57 +0100

    Make list of methods for creation

--- storage/file_storage.py
+++ storage/file_storage.py
@@ -3,7 +3,26 @@
 JSON file storage for Unlearn web app
 
 Modules Imported:
-
+json: serialize and deserialize json file_storage
 
 """
-import JSON
+import json
+
+
+class FileStorage:
+    """Handles storage needs of web app via JSON"""
+    __file_path = 'unlearn_storage.json'
+    __objects = {}
+
+    def all(self):
+        """Loads all objects from staorge"""
+
+    def new(self, obj):
+        """Adds new object to storage dict i.e __objects"""
+
+    def save(self):
+        """Commits new objects in __objects to json storage i.e unlearn_storage.json"""
+
+    def reload(self):
+        """Loads committed objects from json storage"""
+
